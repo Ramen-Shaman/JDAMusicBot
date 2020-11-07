aef509f98e07c5a26700d90110700b87130ca276
Ramen Shaman <Gravatacs@gmail.com>
2020-11-07 17:07:19 -0600

    Updated lavaplayer and JDA to latest releases.

--- src/main/java/Startup.java
+++ src/main/java/Startup.java
@@ -1,20 +1,36 @@
-import TVBot.Messenger.MusicStuff.MessageCommands;
-import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import javax.security.auth.login.LoginException;
 
 
-public class Startup
+public class Startup extends ListenerAdapter
 {
+
     public static void main(String[] args) throws LoginException
     {
-        JDABuilder builder = new JDABuilder(AccountType.BOT);
-        builder.setToken("NjgwNTA5Mzc2OTg0OTA3Nzk2.XlA7vg.pejJ7E6GR7HfA6eOiIx_0Y5mB1Y");
-        builder.addEventListeners(new MessageCommands());
-        builder.setActivity(Activity.watching("The Election"));
+
+        String token = "NjgwNTA5Mzc2OTg0OTA3Nzk2.XlA7vg.pejJ7E6GR7HfA6eOiIx_0Y5mB1Y";
+
+        JDABuilder builder = JDABuilder.createDefault(token);
+        builder.setActivity(Activity.watching("Three's Company"));
         builder.build();
     }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        String message =  event.getMessage().toString();
+        String[] command = message.split(" ", 0);
+        if(command[0].equals("~play") && command[1].isEmpty())
+        {
+            event.getChannel().sendMessage("Please enter an argument!").queue();
+        }
+        System.out.println(command[0]);
+    }
+
 
 }
 
